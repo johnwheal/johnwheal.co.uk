ef61f5f7ae35e0b9bf5394339d595cc038be49ef
John Wheal <john@wheal.co.uk>
2022-12-23 12:48:12 +0000

    Fixed build process

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
@@ -13,6 +13,8 @@ var rename = require('gulp-rename');
 
 var clean = require('gulp-clean');
 var replace = require('gulp-replace');
+
+var del = require('del');
 
 var stylesheets = [
     '**/*.scss'
@@ -27,9 +29,6 @@ gulp.task('sass', function () {
             errLogToConsole: true,
             includePaths: stylesheets
         }).on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
-        }))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('css'))
         .pipe(browserSync.stream());
@@ -40,15 +39,15 @@ gulp.task('serve', function() {
     browserSync.init({ server: '.' });
 
     // watch files and build/reload where needed
-    gulp.watch(['**/*.scss'], ['sass']);
+    gulp.watch(['**/*.scss'], gulp.series('sass'));
     gulp.watch('**/*.html').on('change', browserSync.reload);
 });
 
 // when running `gulp build` for a static build
-gulp.task('build', ['sass']);
+gulp.task('build', gulp.series('sass'));
 
 // when running `gulp` to build, watch and re-build
-gulp.task('default', ['build', 'serve']);
+gulp.task('default', gulp.series('build', 'serve'));
 
 var filesToMove = [
     './css/**/*.*',
@@ -57,10 +56,12 @@ var filesToMove = [
     './webfonts/**/*.*'
 ];
 
-gulp.task('build-prod', ['sass'], function() {
-    gulp.src('dist', {read: false}).pipe(clean());
+gulp.task('build-prod', gulp.series('sass', async function() {
+    del(['dist/**', '!dist'], {force:true});
+
+    gulp.src('dist', {read: false, allowEmpty: true}).pipe(clean());
 
     gulp.src(filesToMove, { base: './' }).pipe(gulp.dest('dist'));
 
     gulp.src(['index.html']).pipe(replace('{TIMESTAMP}', Date.now())).pipe(gulp.dest('dist/'));
-});
+}));
